b8ae120fc1e6a2b2214eca212414a22392f95580
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-19 17:35:13 -0500

    Creating a new class with extending constructor

--- src/app.ts
+++ src/app.ts
@@ -11,14 +11,27 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
-  return function (constructor: any) {
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T
+  ) {
     console.log("Rendering template");
-    const hookEl = document.getElementById(hookId);
-    const p = new constructor();
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      hookEl.querySelector("h1")!.textContent = p.name;
-    }
+    // const hookEl = document.getElementById(hookId);
+    // const p = new originalConstructor();
+    // if (hookEl) {
+    //   hookEl.innerHTML = template;
+    //   hookEl.querySelector("h1")!.textContent = p.name;
+    // }
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
+        console.log("New Class");
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector("h1")!.textContent = this.name;
+        }
+      }
+    };
   };
 }
 @Logger("Logging - Default")
